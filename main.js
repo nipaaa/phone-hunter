601bbf1ef5c1e34f95e3abae1940e098100974f9
Nipa <akternipa941@gmail.com>
2022-03-01 22:38:12 +0600

    error handle

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
+const main = document.getElementById('main')
+const sms = document.getElementById('sms');
+
 // search phone
 const searchPhone = () => {
     const searchBox = document.getElementById('search-box');
     const searchText = searchBox.value;
     searchBox.value = '';
     if (searchText == '') {
-       const sms = document.getElementById('sms');
        sms.innerHTML=`
        <h3>Please search your favourite phone by name</h3>`
+       main.style.display = 'none'
     } 
     else {
+        sms.innerHTML = '';
+        main.style.display = 'block'
         const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
         fetch(url)
         .then(res => res.json())
         .then(phone => displayPhone(phone.data)); 
-    }
+
+    } 
    
 }
 
@@ -23,7 +29,6 @@ const displayPhone = phones => {
     const displayDiv = document.getElementById('display-phone');
     displayDiv.textContent = '';
     if (phones.length == 0) {
-        const sms = document.getElementById('sms');
         sms.innerHTML=`
         <h3>This name is not Found.Please search phone by using a valid name</h3>`
     }
@@ -44,6 +49,8 @@ const displayPhone = phones => {
             displayDiv.appendChild(div); 
         });   
     }
+    const empty2 = displayPhoneDetails()
+    empty2.textContent = '';
   
 }
 
@@ -59,21 +66,17 @@ const loadPhoneDetails = phoneId => {
 
 // display details
 const displayPhoneDetails = phone => {
-    console.log(phone);
     const detailsDiv = document.getElementById('display-details');
     detailsDiv.textContent = '';
     const div = document.createElement('div');
     div.classList.add('row');
-    // div.classList.add('bg-light');
-    // div.classList.add('py-3');
-    // div.classList.add('my-5');
     div.innerHTML = `
     <div class="col-lg-5 col-sm-12 d-flex justify-content-center align-items-center"> <img src="${phone.image}" class="card-img-top w-50 mx-auto" alt="..."></div>
    
 
     <div class="card-body col-lg-7 col-sm-12">
     <h4 class="card-title">Name: ${phone.name}</h4>
-    <h5 class="card-title">${phone.releaseDate ? phone.releaseDate : 'Released date not found'} </h5>
+    <h5 class="card-title">${phone.releaseDate ? phone.releaseDate : 'Released date not found'}</h5>
     <p class="card-text">
     <span class="fw-bolder"> Main Features:</span> <br />
     <span class="fw-bold">Storage: </span> ${phone.mainFeatures.storage} <br />
@@ -84,12 +87,12 @@ const displayPhoneDetails = phone => {
     <span class="fw-bold">sensors: </span> ${phone.mainFeatures.sensors} <br /><br />
 
     <span class="fw-bolder">Others:</span> <br />
-    <span class="fw-bold">WLAN: </span> ${phone.others.WLAN} <br />
-    <span class="fw-bold">Bluetooth: </span> ${phone.others.Bluetooth} <br />
-    <span class="fw-bold">GPS: </span> ${phone.others.GPS} <br />
-    <span class="fw-bold">NFC: </span> ${phone.others.NFC} <br />
-    <span class="fw-bold">Radio: </span> ${phone.others.Radio} <br />
-    <span class="fw-bold">USB: </span> ${phone.others.USB} <br />
+    <span class="fw-bold">WLAN: </span> ${phone.others? phone.others.WLAN : 'not found' } <br />
+    <span class="fw-bold">Bluetooth: </span> ${phone.others? phone.others.Bluetooth : 'not found'} <br />
+    <span class="fw-bold">GPS: </span> ${phone.others? phone.others.GPS : 'not found'} <br />
+    <span class="fw-bold">NFC: </span> ${phone.others? phone.others.NFC : 'not found'} <br />
+    <span class="fw-bold">Radio: </span> ${phone.others? phone.others?.Radio : 'not found'} <br />
+    <span class="fw-bold">USB: </span> ${phone.others? phone.others?.USB  : 'not found'} <br />
 
     </p>
    
