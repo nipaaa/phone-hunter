56f54917397010b4de96d613fdc1d68f94e64105
Nipa <akternipa941@gmail.com>
2022-03-01 20:21:36 +0600

    error handling

--- main.js
+++ main.js
@@ -35,13 +35,14 @@ const displayPhone = phones => {
             div.innerHTML = `<div class="card py-4 bg-light">
             <img src="${phone.image}" class="card-img-top w-50 mx-auto" alt="...">
             <div class="card-body text-center">
-             <h5 class="card-title">${phone.phone_name}</h5>
-            <p class="card-text">${phone.brand}</p>
+             <h5 class="card-title">Name: ${phone.phone_name}</h5>
+            <p class="card-text">Brand: ${phone.brand}</p>
             <button onclick="loadPhoneDetails('${phone.slug}')" type="button" class="fw-bold btn btn-secondary">Show Details</button>
             </div>
+            </div>
             `;
-            displayDiv.appendChild(div);
-        });
+            displayDiv.appendChild(div); 
+        });   
     }
   
 }
@@ -62,23 +63,38 @@ const displayPhoneDetails = phone => {
     const detailsDiv = document.getElementById('display-details');
     detailsDiv.textContent = '';
     const div = document.createElement('div');
-    div.classList.add('card');
-    div.classList.add('bg-light');
-    div.classList.add('py-3');
-    div.classList.add('my-5');
-    const realeaseDate = phone.realeaseDate;
+    div.classList.add('row');
+    // div.classList.add('bg-light');
+    // div.classList.add('py-3');
+    // div.classList.add('my-5');
     div.innerHTML = `
-    <img src="${phone.image}" class="card-img-top w-50 mx-auto" alt="...">
-    <div class="card-body text-center">
-    if(realeaseDate == ''){
-        <h5 class="card-title">none</h5>  
-    }else{
-        <h5 class="card-title">${phone.releaseDate}</h5>
-    }
-      
-        <p class="card-text"></p>
-       
-    </div>
+    <div class="col-lg-5 col-sm-12 d-flex justify-content-center align-items-center"> <img src="${phone.image}" class="card-img-top w-50 mx-auto" alt="..."></div>
+   
+
+    <div class="card-body col-lg-7 col-sm-12">
+    <h4 class="card-title">Name: ${phone.name}</h4>
+    <h5 class="card-title">${phone.releaseDate ? phone.releaseDate : 'Released date not found'} </h5>
+    <p class="card-text">
+    <span class="fw-bolder"> Main Features:</span> <br />
+    <span class="fw-bold">Storage: </span> ${phone.mainFeatures.storage} <br />
+    <span class="fw-bold"> Display Size: </span>${phone.mainFeatures.displaySize} <br />
+    <span class="fw-bold">Chip Set: </span> ${phone.mainFeatures.chipSet} <br />
+    <span class="fw-bold">Memory: </span> ${phone.mainFeatures.memory} <br /> <br />
+
+    <span class="fw-bold">sensors: </span> ${phone.mainFeatures.sensors} <br /><br />
+
+    <span class="fw-bolder">Others:</span> <br />
+    <span class="fw-bold">WLAN: </span> ${phone.others.WLAN} <br />
+    <span class="fw-bold">Bluetooth: </span> ${phone.others.Bluetooth} <br />
+    <span class="fw-bold">GPS: </span> ${phone.others.GPS} <br />
+    <span class="fw-bold">NFC: </span> ${phone.others.NFC} <br />
+    <span class="fw-bold">Radio: </span> ${phone.others.Radio} <br />
+    <span class="fw-bold">USB: </span> ${phone.others.USB} <br />
+
+    </p>
+   
+</div>
+  
     `;
     detailsDiv.appendChild(div);
 }
