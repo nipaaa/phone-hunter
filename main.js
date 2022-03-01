467d1900ab316ee3b1637a397845c4aafff1984d
Nipa <akternipa941@gmail.com>
2022-03-01 14:35:13 +0600

    update

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ const searchPhone = () => {
     const searchText = searchBox.value;
     searchBox.value = '';
     if (searchText == '') {
-       alert('np')
+       const sms = document.getElementById('sms');
+       sms.innerHTML=`
+       <h3>Please search your favourite phone by name</h3>`
     } 
     else {
         const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
@@ -20,19 +22,28 @@ const searchPhone = () => {
 const displayPhone = phones => {
     const displayDiv = document.getElementById('display-phone');
     displayDiv.textContent = '';
-    phones.forEach(phone => {
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML = `<div class="card py-4 bg-light">
-        <img src="${phone.image}" class="card-img-top w-50 mx-auto" alt="...">
-        <div class="card-body text-center">
-         <h5 class="card-title">${phone.phone_name}</h5>
-        <p class="card-text">${phone.brand}</p>
-        <button onclick="loadPhoneDetails('${phone.slug}')" type="button" class="fw-bold btn btn-secondary">Show Details</button>
-        </div>
-        `;
-        displayDiv.appendChild(div);
-    });
+    if (phones.length == 0) {
+        const sms = document.getElementById('sms');
+        sms.innerHTML=`
+        <h3>This name is not Found.Please search phone by using a valid name</h3>`
+    }
+    else{
+        const first20Phones = phones.slice(0,20);
+        first20Phones.forEach(phone => {
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `<div class="card py-4 bg-light">
+            <img src="${phone.image}" class="card-img-top w-50 mx-auto" alt="...">
+            <div class="card-body text-center">
+             <h5 class="card-title">${phone.phone_name}</h5>
+            <p class="card-text">${phone.brand}</p>
+            <button onclick="loadPhoneDetails('${phone.slug}')" type="button" class="fw-bold btn btn-secondary">Show Details</button>
+            </div>
+            `;
+            displayDiv.appendChild(div);
+        });
+    }
+  
 }
 
 
@@ -47,6 +58,7 @@ const loadPhoneDetails = phoneId => {
 
 // display details
 const displayPhoneDetails = phone => {
+    console.log(phone);
     const detailsDiv = document.getElementById('display-details');
     detailsDiv.textContent = '';
     const div = document.createElement('div');
@@ -54,10 +66,16 @@ const displayPhoneDetails = phone => {
     div.classList.add('bg-light');
     div.classList.add('py-3');
     div.classList.add('my-5');
+    const realeaseDate = phone.realeaseDate;
     div.innerHTML = `
     <img src="${phone.image}" class="card-img-top w-50 mx-auto" alt="...">
     <div class="card-body text-center">
+    if(realeaseDate == ''){
+        <h5 class="card-title">none</h5>  
+    }else{
         <h5 class="card-title">${phone.releaseDate}</h5>
+    }
+      
         <p class="card-text"></p>
        
     </div>
